28791f33bd813790ece116c5e08cb0d576830593
D'Ann Hart <D'Ann Hart>
2016-11-15 17:54:08 -0600

    working game, except after 'new game'

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,17 @@
-
-
+//initial variables
 //creature count so we know when we have defeated eveyone
 var creatureDef = 0;
 var playerSet = false;
 var opponentSet=false;
 var player = "";
 var attacker ="";
-
 var clickCounter = 0;
-// Creature list
-//var elfWizard = new creature("Elf Wizard", 97, 5, 12);
-//var dragonSorc = new creature("Dragonborn Sorcerer", 110, 5, 8);
-//var humanPaladin = new creature("Human Paladin", 120, //7, 6);
-//var humanDruid = new creature("Human Druid", 140, 5, 11);
 
+
+//new game button - only shows at win/lose
+var resetButton = $("<div class='panel-footer'><button class='btn btn-default' id='reset'>New Game</button></div>");
+
+// Creature list
 var creatureList = {
 	 elfWizard: ["Elf Wizard", 97, 5, 12],
 	 dragonSorc: ["Dragonborn Sorcerer", 110, 5, 8],
@@ -21,6 +19,9 @@ var creatureList = {
 	 humanPaladin:["Human Paladin", 120, 7, 6],
 	 humanDruid: ["Human Druid", 140, 5, 11]
 };
+
+//displays inital board setup - characters with initial values, etc.
+function setup(){
 
 $.each(creatureList, function(key, value) {
 //for (var i=0; i<creatureList.length; i++){
@@ -30,17 +31,18 @@ $.each(creatureList, function(key, value) {
 	  var creatureId = key;
 	  var creatureName= value[0];
 	  var creatureHP = value[1];
-	var newDiv= $("<div class='panel panel-default creatureLink' id='" +creatureId +  "' value='" +creatureId+"'><div class='panel-body'><img src='assets/images/" + creatureId+".jpg' class='img-responsive' alt='"+creatureName+"' /><div class='panel-footer'><h3>"+creatureName+"</h3><p class='"+creatureId+"hp'>Hit Points = "+creatureHP+"</p></div></div>");
+	var newDiv= $("<div class='panel panel-default creatureLink' id='" +creatureId +  "' value='" +creatureId+"'><div class='panel-body'><img src='assets/images/" + creatureId+".jpg' class='img-responsive' alt='"+creatureName+"' /></div><div class='panel-footer'><h3>"+creatureName+"</h3><p class='"+creatureId+"hp'>Hit Points = "+creatureHP+"</p></div>");
 
 	creatureBox.append(newDiv);
-
-
 });
-var creatureBox=$("#creatureBox");
+};
+
+//setup the board
+setup();
 
 //on.click the creatureLink, check to see if attacker has been made, if not, make creature and assign it to attacker
-
-$(".creatureLink").click(function(event) {
+$(".creatureLink").on("click", setupPlayer ); 
+function setupPlayer() {
 	var self = this.attributes[1].value; 
 		//var creatureId = key;
 	  	//var creatureName= value[0];
@@ -61,7 +63,7 @@ $(".creatureLink").click(function(event) {
 
 		//move the character to the attackerBox
 		var creatureId = "#"+self;
-		$(creatureId).appendTo("#attackerBox");
+		$(creatureId).appendTo("#attackerHolder");
 		$("p."+self+"hp").addClass( "attacker" );
 
 
@@ -78,15 +80,20 @@ $(".creatureLink").click(function(event) {
 	
 	
 } else if (!opponentSet) {
+	if (player === self) {
+		alert("You cannot fight yourself.");
+		return;
+	} else {
+
 	//assign as defender
-		//set character as the player's character
+		//set character as the opponent's character
 		opponent =self;
 		console.log(opponent);
 
 		//change instructions
 		$( "#creatureHeading" ).text( "Click Attack to fight your opponent.");
 
-		//move the character to the opponentrBox
+	//move the character to the opponentBox
 		var creatureId = "#"+self;
 		$(creatureId).appendTo("#opponentBox");
 		$("p."+self+"hp").addClass( "defender" );
@@ -103,40 +110,28 @@ $(".creatureLink").click(function(event) {
 		opponentSet = true;
 
 		//code for attack button here
-		var attackButton = $("<div id='attackButton'><button class='btn btn-default'>ATTACK!<br /><span class='orange-circle-greater-than'></span></button></div>");
-
-		$("#attackerBox").append(attackButton);
-
-		$("#attackerBox").bind('attack', function() {
-	    //attacker strikes at the defender
-			clickCounter++
-			defender.hp = defender.hp - (attacker.att * clickCounter);
-			
-			//defender counters
-			attacker.hp = attacker.hp - defender.counter;
-
-			$("p.defender").text("Hit Points = "+defender.hp);
-			$("p.attacker").text("Hit Points = "+attacker.hp);
-		//checkDefender();
+		var attackButton = $("<div id='attackButton' class='panel panel-default'><div class='panel-heading' ><button class='btn btn-default' id='attack'>ATTACK!</button></div> <div class='panel-body'><p id='attackMessage'</p></div></div>");
 
 
-});
+		$("#attackElements").html(attackButton);
+
+		$("#attack").on("click", attack ); 
 
 
-$("#attackerBox").click(function(){
-    $(this).trigger('attack');
+//$("#attackerButton").click(function(){
+    //$(this).trigger('attack');
 
 
 
 
 
-});
+};
   
 } else {
 	alert("You already have an opponent.");
 };
 
-}); //end onclick creatureLink
+}; //end onclick creatureLink
 
 
 
@@ -148,47 +143,112 @@ function creature (name, hp, att, counter) {
 		this.counter = counter;
 };
 
+function attack(){
 
-
-//function attack(){
-		//attacker strikes at the defender
-		//newAtt = newAtt + attacker.att;
-		//defender.hp = defender.hp - newAtt;
-		
-		//defender counters
-		//attacker.hp = attacker.hp - defender.counter;
-
-	//checkDefender();
-//};
-//function to create the chosen attacker
-//var player = new attacker(elfWizard);
-//attacker.creature.hp 
+	    //attacker strikes at the defender
+			clickCounter++
+			defender.hp = defender.hp - (attacker.att * clickCounter);
+			
+			//defender counters
+			attacker.hp = attacker.hp - defender.counter;
 
 
 
-//function to assign defender
-function defender (creature){
-		this.creature= creature;
+		checkDefender();
+
 };
-
-
 
 
 //Is the defender still alive?
 function checkDefender() {
-	//check to see if defender's hp is > 0
-	if (defender.hp <= 0) {
+		
+	//update hp
+		$("p.defender").text("Hit Points = "+defender.hp);
+		$("p.attacker").text("Hit Points = "+attacker.hp);
+
+
+	//check to see if you are still alive
+	if (attacker.hp >= 0){
+
+		//check to see if defender is still alive
+
+		//if defender is dead
+		if (defender.hp <= 0) {
+			
 		//enter code in to move defender to the defeated area and choose another character to fight, update creature count
-		creatureDef = creatureDef + 1;
-			if (creatureDef < 5) {
-				return;
+		creatureDef++;
+			//check to see if you have defeated everyone
+			if (creatureDef < 4) {
+				//display
+				$("#attackMessage").html("<p>You hit your opponent for "+(attacker.att * clickCounter)+" damage. They hit you for "+defender.counter+" damage.</p> <p><strong>You have defeated your opponent!</strong><br /> Please choose another from the Creature Box.</p>");
+				$( "#creatureHeading" ).text( "Choose your next opponent.");
+				$("#attackElements").empty();
+				moveDefender ();
 			} else {
-				//code for you win
+				//you have defeated everyone
+				$("#attackMessage").html("<p>You hit your opponent for "+(attacker.att * clickCounter)+" damage. They hit you for "+defender.counter+" damage.</p> <p><strong>You WIN!</strong></p>");
+				$("#attackElements").empty();
+				moveDefender ();
+
+				//code for new game button here
+				var resetButton = $("<div class='panel panel-default'><button class='btn btn-default' id='reset'>New Game</button></div>");
+				$("#attackElements").append(resetButton);
+				$("#reset").on("click", reset ); 
+
 			};
-	} else {
-		//enter code for diplaying "You hit the defender.name for newAttack damage. They hit you for counter damage." And display the new HP
-		return;
-	};
-}; //checkDefender close
+		} else {
+		//defender is still alive
+			$("#attackMessage").html("<p>You hit your opponent for "+(attacker.att * clickCounter)+" damage. They hit you for "+defender.counter+" damage.</p>");
+			return;
+		};
+ 	} else {
+ 	//You are dead
+ 	$("#attackMessage").html("<p>You hit your opponent for "+(attacker.att * clickCounter)+" damage. They hit you for "+defender.counter+" damage.</p> <p><strong>You have been defeated!</strong></p>");
+ 		$("#attackElements").empty();
+				//code for new game button here
+				var resetButton = $("<div class='panel-footer'><button class='btn btn-default' id='reset'>New Game</button></div>");
+				$("#attackButton").append(resetButton);
+				$("#reset").on("click", reset ); 
+ } 
+
+};//checkDefender close
+
+function moveDefender(){
+				var creatureId = "#"+opponent;
+				$(creatureId).appendTo("#defeatedBox");
+				$("p."+opponent+"hp").removeClass( "defender" );
+				opponentSet = false;
+}
+
+//new game reset function
+function reset (){	
+	//remove creature from defeated and attacker areas
+
+	$( "#attackerHolder" ).remove(".creatureLink");
+	$( "#opponentBox" ).remove(".creatureLink");
+	$( "#defeatedBox" ).empty();
+
+	$( "#attackerHolder" ).empty();
+	$( "#attackElements" ).empty();
+
+	$( "#creatureHeading" ).text( "Choose a character to begin.");
+	//reset global variables
+			creatureDef = 0;
+			playerSet = false;
+			opponentSet=false;
+			player = "";
+			attacker ="";
+			clickCounter = 0;
+	//reset the board
+			setup();
+			
+			$("#reset").off("click", "**" );
+			$("#attack").off("click",  "**"  ); 
+}; //end function reset
 
 
+
+
+
+
+	
